4818bf645440f2bec6d4f4982be2f2652682490d
vanthoivo <vovanthoi02011998@gmail.com>
2026-04-13 15:20:56 +0200

    complete compare.py and recommend.py with core basket logic

--- src/compare.py
+++ src/compare.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from load_data import load_offers
 
 def compare_basket(shopping_list, offers_df):
@@ -10,17 +9,25 @@ def compare_basket(shopping_list, offers_df):
     :return:
         pd.DataFrame: dataFrame: basket total per store
     """
+    # offers_df[offers_df["normalized_name"].isin(shopping_list)]: select the column named "normalized_name",
+    # then check if each value in the column inside the shopping_list -> return True / False
+    # then keep only the row where it returns True value
+    # copy(): copy the filtered DataFrame -> make it safer to work with the result
+
+    # => we get a smaller table containing only thr products the user wants
+
     matched_offers = offers_df[offers_df["normalized_name"].isin(shopping_list)].copy()
 
     basket_totals = (
-        matched_offers.groupby("store_id")["price"]
-        .sum()
-        .reset_index()
+        matched_offers.groupby("store_id")["price"] # group the filter DataFrame by store_id, then look at the prices only
+        .sum()                                      # sum the prices in each group
+        .reset_index() # when using sum((). mean(), count(), etc after groupby(), the groupby() column become the index of the aggregated result
+                       # therefore, we need to reset_index(), to build another index column, and return groupby() to be the normal column
         .rename(columns={"price": "basket_total"})
     )
     return matched_offers, basket_totals
 
-if __name__ == "__main__":
+if __name__ == "__main__": # run the code below only if this file is executed directly
     offers_df = load_offers()
 
     shopping_list = ["milk", "eggs", "pasta"]
--- src/recommend.py
+++ src/recommend.py
@@ -1,1 +1,33 @@
+from src.load_data import load_stores, load_offers
+from src.compare import compare_basket
 
+
+def recommend_cheapest_store(shopping_list):
+    stores_df = load_stores()
+    offers_df = load_offers()
+
+    matched_offers, basket_totals = compare_basket(shopping_list, offers_df)
+
+    merged_result = basket_totals.merge(stores_df, on = "store_id", how = "left") # keep all row from the left table (basket_totals),
+                                                                                  # find matching rows in the right table (stores_df)
+                                                                                  # and attach the other store columns
+    cheapest_store = merged_result.sort_values("basket_total").iloc[0] # sort store from cheapest to most expensive, then pick the 1st one
+
+    return matched_offers, merged_result, cheapest_store
+
+if __name__ == "__main__":
+    shopping_list = ["milk", "eggs", "pasta"]
+    match_offers, full_result, cheapest = recommend_cheapest_store(shopping_list)
+
+    print("Shopping list:", shopping_list)
+    print("\nMatched offers:")
+    print(match_offers)
+
+    print("\nBasket totals with store info:")
+    print(full_result[["store_id", "name", "basket_total"]])
+
+    print("\nRecommendations:")
+    print(
+        f"The cheapest store is {cheapest['name']}"
+        f" with a basket total of €{cheapest['basket_total']:.2f}."
+    )
